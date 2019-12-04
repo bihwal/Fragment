dbf6bbd7e16fae07258d0302cfa27a47c38d7d41
bihwal <strength147@gmail.com>
2019-12-04 10:45:06 +0545

    making two fragments

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnFragment;
+    private Boolean staus=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragment=findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
     }
 }
